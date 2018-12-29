bff528f7c5e41e09205ad445ff87c53c6e178698
yambal <june@yambal.net>
2018-12-29 14:18:04 +0900

    XI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # has_secure_passwordメソッドを追加してください
+  has_secure_password
+
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
-  validates :password, {presence: true}
+  # 以下のバリデーションを削除してください
+
 
   def posts
     return Post.where(user_id: self.id)
